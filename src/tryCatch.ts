3ce02f2c429777507ff16681b3044cf05bfc2ecf
出口レイ <richard@kanshen.click>
2025-03-24 20:29:26 +0100

    slight types improvement

--- src/tryCatch.ts
+++ src/tryCatch.ts
@@ -12,13 +12,13 @@ type Failure<E> = {
 type Result<T, E = Error> = Success<T> | Failure<E>;
 
 // Main wrapper function
-export async function tryCatch<T, E = Error>(
+export default async function tryCatch<T, E = Error>(
     promise: Promise<T>,
 ): Promise<Result<T, E>> {
     try {
         const data = await promise;
-        return { data, error: null };
+        return { data, error: null } as Success<T>;
     } catch (error) {
-        return { data: null, error: error as E };
+        return { data: null, error } as Failure<E>;
     }
 }
